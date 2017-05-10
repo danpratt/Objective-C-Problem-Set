a6d31fc5d4a80922e1cb39bbb0c4f1c88b900e21
Daniel Pratt <daniel@blaumagier.com>
2017-05-10 16:22:08 -0700

    feat: completed GuitarString problem set
    - able to check for and display an error

--- GuitarString/GuitarString/main.m
+++ GuitarString/GuitarString/main.m
@@ -13,10 +13,17 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
         // a. Create an instance of the class GuitarString
+        GuitarString * string = [[GuitarString alloc] init];
         // b. Create an NSError
+        NSError * error;
         // c. Call the method pluck(velocity: Float)
+        [string pluck:1.7 error:&error];
         // d. Check if an error was returned
-        // e. Log an error if one was returned
+        if (error != nil) {
+            // e. Log an error if one was returned
+            NSLog(@"Oh no, an error: %@", error);
+        }
+        
 
     }
     return 0;
